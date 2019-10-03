1084b7ba59a0ad5e9b81ba26ee88834720bde89d
risk0 <risk0@o2.pl>
2019-10-03 03:34:27 +0200

    isPalindrome method added

---
+++ palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void isPalindrome(string str)
+        {
+            Regex rgx = new Regex("[^a-zA-Z]");
+            string newStr = rgx.Replace(str, "");
+            newStr = newStr.ToLower();
+
+            Console.WriteLine(newStr.SequenceEqual(newStr.Reverse()) ? "YES" : "NO");
+        }
+
+
+        static void Main(string[] args)
+        {
+            isPalindrome("abut-1-tuba");
+            isPalindrome("@allula");
+        }
+    }
+}
