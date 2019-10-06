62bdc4bb55f51fcdaf5d48191b1e5a8a5b9034bc
risk0 <risk0@o2.pl>
2019-10-06 23:36:17 +0200

    Existing powers input

---
+++ existing_powers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace existing_powers
+{
+    class Program
+    {
+        static int[] getInput()
+        {
+            var numbers = new List<int>();
+            string s;
+            while (!string.IsNullOrEmpty(s = Console.ReadLine()))
+            {
+                numbers.Add(int.Parse(s));
+            }
+
+            return numbers.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            int[] x = getInput();
+
+            foreach (int i in x)
+            {
+                System.Console.Write("{0} ", i);
+            }
+        }
+    }
+}
